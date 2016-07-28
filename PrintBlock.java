86fa14e99b9fe5c985a388380f28a0d8ef876343
liltove <kringlan@uci.edu>
2016-07-27 19:04:22 -0700

    created skeleton new print block

---
+++ PrintBlock.java
@@ -0,0 +1,10 @@
+package com.printfromminecraft;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class PrintBlock extends Block {
+    public PrintBlock() {
+        super(Material.ROCK);
+    }
+}

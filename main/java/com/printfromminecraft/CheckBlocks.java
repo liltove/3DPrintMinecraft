20e6637dc4aa67c044bbc314e9cfb72a84b76374
liltove <kringlan@uci.edu>
2016-07-30 16:03:11 -0700

    added function to check if block is air

--- main/java/com/printfromminecraft/CheckBlocks.java
+++ main/java/com/printfromminecraft/CheckBlocks.java
@@ -2,6 +2,8 @@ package com.printfromminecraft;
 
 import java.util.ArrayList;
 import java.util.List;
+import net.minecraft.world.World;
+import net.minecraft.util.math.BlockPos;
 
 
 
@@ -22,7 +24,7 @@ public class CheckBlocks {
     public static List<SolidBlockPositions> printObject = new ArrayList();
     public static List<Double> widthHeightDepth = new ArrayList();
     
-    public CheckBlocks(){
+    public CheckBlocks(World world){
         //temporarily initializing the lists here so we don't have to call the wand function
         //FEEL FREE TO CHANGE THESE NUMBERS
         pos1.add(0.0);
@@ -33,10 +35,10 @@ public class CheckBlocks {
         pos2.add(5.0);
         
         //call the function that will check the matrix cube
-        loopThruCoords(pos1, pos2);
+        loopThruCoords(pos1, pos2, world);
     }
     
-    public void loopThruCoords(List<Double> p1, List<Double> p2){
+    public void loopThruCoords(List<Double> p1, List<Double> p2, World world){
         //initialize the x, y, z -- these will chage as going through the matrix cube
         double x = 0.0;
         double y = 0.0;
@@ -47,13 +49,17 @@ public class CheckBlocks {
         //to access x => p1.get(0), y => p1.get(1), z => p1.get(2)
         
         //to call checkBlock
-        checkBlock(x,y,z);
+        checkBlock(x,y,z, world);
     }
     
-    public void checkBlock(double x, double y, double z){
+    public void checkBlock(double x, double y, double z, World world){
         //get block for the coordiantes at (x,y,z)
-        
         //check if that block is air or not
+        BlockPos block = new BlockPos((int)x, (int)y-2, (int)z);
+        if (world.getBlockState(block).getBlock().isAir(world.getBlockState(block), world, block)) {
+            //it's air, do nothing
+            return;
+        }
         
         //if not air, add a new solid block position to the array
         printObject.add(new SolidBlockPositions(x, y, z));
@@ -69,18 +75,18 @@ public class CheckBlocks {
             xmin = pos1.get(0);
         }
         if(pos1.get(1)> pos2.get(1)){
-            xmax = pos1.get(1);
-            xmin = pos2.get(1);
+            ymax = pos1.get(1);
+            ymin = pos2.get(1);
         }else{
-            xmax = pos2.get(1);
-            xmin = pos1.get(1);
+            ymax = pos2.get(1);
+            ymin = pos1.get(1);
         }
         if(pos1.get(2)> pos2.get(2)){
-            xmax = pos1.get(2);
-            xmin = pos2.get(2);
+            zmax = pos1.get(2);
+            zmin = pos2.get(2);
         }else{
-            xmax = pos2.get(2);
-            xmin = pos1.get(2);
+            zmax = pos2.get(2);
+            zmin = pos1.get(2);
         }
     }
 }
